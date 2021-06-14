cb6a116f662a6c6c3978a49bb086ecc90c7f63b4
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-06-14 18:50:24 +0530

    added missing package

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    mongoURI: process.env.MONGODB_URI,
+    mongoURI:"mongodb+srv://new-user:Iamhappy2018@@cluster0.whwom.mongodb.net/banking_app?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
